2a09e2167e9ce84e769a9f271da1fba1189939c3
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-08 19:01:29 +0000

    health logic

---
+++ src/health/ping.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { prettyJSON } from '../helpers';
+
+const GET_PING = `
+  query {
+    ping
+  }
+`;
+
+export const pingRequest = () => {
+  return apolloClient.query({
+    query: gql(GET_PING),
+  });
+};
+
+export const ping = async () => {
+  const result = await pingRequest();
+  prettyJSON('ping: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await ping();
+})();
