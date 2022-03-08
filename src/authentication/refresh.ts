e144f948831080abbba78729bcf406c9e966bf69
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-08 17:14:24 +0000

    refresh

---
+++ src/authentication/refresh.ts
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { getAddressFromSigner } from '../ethers.service';
+import { login } from './login';
+
+const REFRESH_AUTHENTICATION = `
+  mutation($request: RefreshRequest!) { 
+    refresh(request: $request) {
+      accessToken
+      refreshToken
+    }
+ }
+`;
+
+export const refreshAuth = (refreshToken: string) => {
+  return apolloClient.mutate({
+    mutation: gql(REFRESH_AUTHENTICATION),
+    variables: {
+      request: {
+        refreshToken,
+      },
+    },
+  });
+};
+
+export const refresh = async () => {
+  const address = getAddressFromSigner();
+  console.log('refresh: address', address);
+
+  // we request a challenge from the server
+  const accessTokens = await login(address);
+
+  const newAccessToken = await refreshAuth(
+    accessTokens.data.authenticate.refreshToken
+  );
+  console.log('refresh: result', newAccessToken);
+
+  return newAccessToken;
+};
+
+(async () => {
+  await refresh();
+})();
