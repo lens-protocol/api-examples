6c76e0e4451198bd989df37a06c5564e0e364348
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-08 23:26:38 +0000

    following

---
+++ src/follow/following.ts
@@ -0,0 +1,136 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { getAddressFromSigner } from '../ethers.service';
+import { prettyJSON } from '../helpers';
+
+const GET_FOLLOWING = `
+  query($request: FollowingRequest!) {
+    following(request: $request) { 
+			    items {
+              id
+              name
+              bio
+              location
+              website
+              twitterUrl
+              handle
+              picture {
+                ... on NftImage {
+                  contractAddress
+                  tokenId
+                  uri
+                  verified
+                }
+                ... on MediaSet {
+                  original {
+                    url
+                    width
+                    height
+                    mimeType
+                  }
+                  medium {
+                    url
+                    width
+                    height
+                    mimeType
+                  }
+                  small {
+                    url
+                    width
+                    height
+                    mimeType
+                  }
+                }
+              }
+              coverPicture {
+                ... on NftImage {
+                  contractAddress
+                  tokenId
+                  uri
+                  verified
+                }
+                ... on MediaSet {
+                  original {
+                    url
+                    width
+                    height
+                    mimeType
+                  }
+                  small {
+                    width
+                    url
+                    height
+                    mimeType
+                  }
+                  medium {
+                    url
+                    width
+                    height
+                    mimeType
+                  }
+                }
+              }
+              ownedBy
+              depatcher {
+                address
+                canUseRelay
+              }
+              stats {
+                totalFollowers
+                totalFollowing
+                totalPosts
+                totalComments
+                totalMirrors
+                totalPublications
+                totalCollects
+              }
+              followModule {
+                ... on FeeFollowModuleSettings {
+                  type
+                  amount {
+                    asset {
+                      name
+                      symbol
+                      decimals
+                      address
+                    }
+                    value
+                  }
+                  recipient
+                }
+            }
+        }
+       pageInfo {
+          prev
+          next
+          totalCount
+       }
+		}
+  }
+`;
+
+export const followingRequest = (walletAddress: string) => {
+  return apolloClient.query({
+    query: gql(GET_FOLLOWING),
+    variables: {
+      request: {
+        address: walletAddress,
+        limit: 10,
+      },
+    },
+  });
+};
+
+export const following = async () => {
+  const address = getAddressFromSigner();
+  console.log('following: address', address);
+
+  const result = await followingRequest(address);
+  prettyJSON('following: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await following();
+})();
