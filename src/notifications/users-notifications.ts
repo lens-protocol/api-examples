9eae6574945e0a280cd1b6ae4d728373d8ca2a70
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-09 12:19:25 +0000

    notifications

---
+++ src/notifications/users-notifications.ts
@@ -0,0 +1,255 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { login } from '../authentication/login';
+import { PROFILE_ID } from '../config';
+import { getAddressFromSigner } from '../ethers.service';
+import { prettyJSON } from '../helpers';
+
+const GET_NOTIFICATIONS = `
+  query($request: NotificationRequest!) { 
+    notifications(request: $request) {
+      items {
+        ... on NewFollowerNotification {
+            ...NewFollowerNotificationFields
+        }
+
+        ... on NewMirrorNotification {
+            ...NewMirrorNotificationFields
+        }
+
+        ... on NewCollectNotification {
+            ...NewCollectNotificationFields
+        }
+
+        ... on NewCommentNotification {
+            ...NewCommentNotificationFields
+        }
+      }
+      pageInfo {
+        ...CommonPaginatedResultInfo
+      }
+  }
+}
+ 
+ fragment CommentWithCommentedPublicationFields on Comment {
+  ...CompactComment
+  commentOn {
+    ... on Post {
+      ...CompactPost
+    }
+    ... on Mirror {
+      ...CompactMirror
+    }
+    ... on Comment {
+      ...CompactComment
+    }
+  }
+}
+
+fragment NewFollowerNotificationFields on NewFollowerNotification {
+  __typename
+  createdAt
+  isFollowedByMe
+  wallet {
+    ...Wallet
+  }
+}
+
+fragment NewCollectNotificationFields on NewCollectNotification {
+  __typename
+  createdAt
+  wallet {
+    ...Wallet
+  }
+  collectedPublication {
+    __typename
+    ... on Post {
+      ...CompactPost
+    }
+
+    ... on Mirror {
+      ...CompactMirror
+    }
+
+    ... on Comment {
+      ...CompactComment
+    }
+  }
+}
+
+fragment NewMirrorNotificationFields on NewMirrorNotification {
+  __typename
+  createdAt
+  profile {
+    ...CompactProfile
+  }
+  publication {
+    ... on Post {
+      ...CompactPost
+    }
+    ... on Comment {
+      ...CompactComment
+    }
+  }
+}
+
+fragment NewCommentNotificationFields on NewCommentNotification {
+  __typename
+  createdAt
+  profile {
+    ...CompactProfile
+  }
+  comment {
+    ...CommentWithCommentedPublicationFields
+  }
+}
+
+fragment CompactProfile on Profile {
+  id
+  name
+  handle
+  picture {
+    ...ProfileMediaFields
+  }
+}
+fragment CompactPublicationStats on PublicationStats {
+  totalAmountOfMirrors
+  totalAmountOfCollects
+  totalAmountOfComments
+}
+
+fragment CompactMetadata on MetadataOutput {
+  name
+  description
+  content
+  media {
+    ...ProfileMediaFields
+  }
+}
+
+fragment CompactPost on Post {
+  id
+  stats {
+    ...CompactPublicationStats
+  }
+  metadata {
+    ...CompactMetadata
+  }
+  profile {
+    ...CompactProfile
+  }
+  collectedBy {
+    ...Wallet
+  }
+  createdAt
+}
+
+fragment CompactMirror on Mirror {
+  id
+  stats {
+    ...CompactPublicationStats
+  }
+  metadata {
+    ...CompactMetadata
+  }
+  profile {
+    ...CompactProfile
+  }
+  createdAt
+}
+
+fragment CompactComment on Comment {
+  id
+  stats {
+    ...CompactPublicationStats
+  }
+  metadata {
+    ...CompactMetadata
+  }
+  profile {
+    ...CompactProfile
+  }
+  collectedBy {
+    ...Wallet
+  }
+  createdAt
+}
+
+fragment CommonPaginatedResultInfo on PaginatedResultInfo {
+  prev
+  next
+  totalCount
+}
+
+fragment MediaFields on Media {
+  url
+  width
+  height
+  mimeType
+}
+
+fragment ProfileMediaFields on ProfileMedia {
+  ... on NftImage {
+    contractAddress
+    tokenId
+    uri
+    verified
+  }
+
+  ... on MediaSet {
+    original {
+      ...MediaFields
+    }
+
+    small {
+      ...MediaFields
+    }
+
+    medium {
+      ...MediaFields
+    }
+  }
+}
+
+fragment Wallet on Wallet {
+  address
+  defaultProfile {
+    ...CompactProfile
+  }
+  totalAmountOfProfiles
+}
+`;
+
+const getNotifications = (profileId: string) => {
+  return apolloClient.mutate({
+    mutation: gql(GET_NOTIFICATIONS),
+    variables: {
+      request: {
+        profileId,
+        limit: 10,
+      },
+    },
+  });
+};
+
+export const notifications = async () => {
+  const profileId = PROFILE_ID;
+  if (!profileId) {
+    throw new Error('Must define PROFILE_ID in the .env to run this');
+  }
+
+  const address = getAddressFromSigner();
+  console.log('notifications: address', address);
+
+  await login(address);
+
+  const result = await getNotifications(profileId);
+
+  prettyJSON('notifications: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await notifications();
+})();
