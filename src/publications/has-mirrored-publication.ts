85d208a5ecfa68d432ef98f4677c1edbba8890d3
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-09 10:25:54 +0000

    publications:has-mirrored-publication

---
+++ src/publications/has-mirrored-publication.ts
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { prettyJSON } from '../helpers';
+
+const HAS_MIRRORED = `
+  query($request: HasMirroredRequest!) {
+    hasMirrored(request: $request) {
+      profileId
+      results {
+        mirrored
+        publicationId
+      }
+    }
+  }
+`;
+
+const hasMirroredRequest = (
+  profilesRequest: { profileId: string; publicationIds: string[] }[]
+) => {
+  return apolloClient.query({
+    query: gql(HAS_MIRRORED),
+    variables: {
+      request: {
+        profilesRequest,
+      },
+    },
+  });
+};
+
+export const hasMirrored = async () => {
+  const result = await hasMirroredRequest([
+    {
+      profileId: '0x032f1a',
+      publicationIds: ['0x032f1a-0x02'],
+    },
+    {
+      profileId: '0x05',
+      publicationIds: ['0x032e32-0x01'],
+    },
+  ]);
+  prettyJSON('has mirrored: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await hasMirrored();
+})();
