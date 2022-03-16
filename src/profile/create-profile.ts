35050c5ce6e2092a8bab3bbb04603d6cfa66eb1d
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-03-16 17:44:02 +0000

    create profile now decodes logs and logs out profile id

--- src/profile/create-profile.ts
+++ src/profile/create-profile.ts
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client/core';
+import { BigNumber, utils } from 'ethers';
 import { apolloClient } from '../apollo-client';
 import { login } from '../authentication/login';
 import { getAddressFromSigner } from '../ethers.service';
@@ -38,16 +39,35 @@ export const createProfile = async () => {
 
   await login(address);
 
-  const result = await createProfileRequest({
+  const createProfileResult = await createProfileRequest({
     handle: new Date().getTime().toString(),
   });
 
-  prettyJSON('create profile: result', result.data);
+  prettyJSON('create profile: result', createProfileResult.data);
 
-  console.log('poll until indexed');
-  await pollUntilIndexed(result.data.createProfile.txHash);
+  console.log('create profile: poll until indexed');
+  const result = await pollUntilIndexed(createProfileResult.data.createProfile.txHash);
 
-  console.log('profile has been indexed');
+  console.log('create profile: profile has been indexed', result);
+
+  const logs = result.txReceipt.logs;
+
+  console.log('create profile: logs', logs);
+
+  const topicId = utils.id(
+    'ProfileCreated(uint256,address,address,string,string,address,bytes,string,uint256)'
+  );
+  console.log('topicid we care about', topicId);
+
+  const profileCreatedLog = logs.find((l: any) => l.topics[0] === topicId);
+  console.log('profile created log', profileCreatedLog);
+
+  let profileCreatedEventLog = profileCreatedLog.topics;
+  console.log('profile created event logs', profileCreatedEventLog);
+
+  const profileId = utils.defaultAbiCoder.decode(['uint256'], profileCreatedEventLog[1])[0];
+
+  console.log('profile id', BigNumber.from(profileId).toHexString());
 
   return result.data;
 };
