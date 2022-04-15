c161daa0ba8036970844c255d9277533be53beb2
damarnez <dani.elnota@gmail.com>
2022-04-15 12:21:19 +0200

    add update profile metadata

--- src/profile/set-update-profile-metadata.ts
+++ src/profile/set-update-profile-metadata.ts
@@ -6,9 +6,10 @@ import { PROFILE_ID } from '../config';
 import { getAddressFromSigner, signedTypeData, splitSignature } from '../ethers.service';
 import { pollUntilIndexed } from '../indexer/has-transaction-been-indexed';
 import { uploadIpfs } from '../ipfs';
-import { lensHub } from '../lens-hub';
+import { lensHub, lensPeriphery } from '../lens-hub';
 import { v4 as uuidv4 } from 'uuid';
 import { ProfileMetadata } from '../interfaces/profile-metadata';
+
 const CREATE_SET_PROFILE_METADATA_TYPED_DATA = `
   mutation($request: CreatePublicSetProfileMetadataURIRequest!) { 
     createSetProfileMetadataTypedData(request: $request) {
@@ -16,36 +17,30 @@ const CREATE_SET_PROFILE_METADATA_TYPED_DATA = `
       expiresAt
       typedData {
         types {
-          SetProfileMetadataUTIWithSig {
+          SetProfileMetadataURIWithSig {
             name
             type
           }
         }
-      domain {
-        name
-        chainId
-        version
-        verifyingContract
+        domain {
+          name
+          chainId
+          version
+          verifyingContract
+        }
+        value {
+          nonce
+          deadline
+          profileId
+          metadata
+        }
       }
-      value {
-        nonce
-        deadline
-        profileId
-        profileIdPointed
-        pubIdPointed
-        contentURI
-        collectModule
-        collectModuleData
-        referenceModule
-        referenceModuleData
-      }
-     }
-   }
- }
+    }
+  }
 `;
 
 // TODO types
-const createSetUpdateProfileTypedData = (createCommentTypedDataRequest: any) => {
+const createSetProfileMetadataTypedData = (createCommentTypedDataRequest: any) => {
   return apolloClient.mutate({
     mutation: gql(CREATE_SET_PROFILE_METADATA_TYPED_DATA),
     variables: {
@@ -54,7 +49,7 @@ const createSetUpdateProfileTypedData = (createCommentTypedDataRequest: any) => 
   });
 };
 
-export const setUpdateProfile = async () => {
+export const setProfileMetadataTypedData = async () => {
   const profileId = PROFILE_ID;
   if (!profileId) {
     throw new Error('Must define PROFILE_ID in the .env to run this');
@@ -92,29 +87,13 @@ export const setUpdateProfile = async () => {
   // hard coded to make the code example clear
   const createProfileMetadataRequest = {
     profileId,
-    // remember it has to be indexed and follow metadata standards to be traceable!
-    publicationId: `0x13-0x3e`,
-    contentURI: 'ipfs://' + ipfsResult.path,
-    collectModule: {
-      // timedFeeCollectModule: {
-      //   amount: {
-      //     currency: currencies.enabledModuleCurrencies.map((c: any) => c.address)[0],
-      //     value: '0.01',
-      //   },
-      //   recipient: address,
-      //   referralFee: 10.5,
-      // },
-      revertCollectModule: true,
-    },
-    referenceModule: {
-      followerOnlyReferenceModule: false,
-    },
+    metadata: 'ipfs://' + ipfsResult.path,
   };
 
-  const result = await createSetUpdateProfileTypedData(createProfileMetadataRequest);
-  console.log('create profile: createSetUpdateProfileTypedData', result);
+  const result = await createSetProfileMetadataTypedData(createProfileMetadataRequest);
+  console.log('create profile: createSetProfileMetadataTypedData', result);
 
-  const typedData = result.data.createSetUpdateProfileTypedData.typedData;
+  const typedData = result.data.createSetProfileMetadataTypedData.typedData;
   console.log('create profile: typedData', typedData);
 
   const signature = await signedTypeData(typedData.domain, typedData.types, typedData.value);
@@ -122,15 +101,10 @@ export const setUpdateProfile = async () => {
 
   const { v, r, s } = splitSignature(signature);
 
-  const tx = await lensHub.commentWithSig({
-    profileId: typedData.value.profileId,
-    contentURI: typedData.value.contentURI,
-    profileIdPointed: typedData.value.profileIdPointed,
-    pubIdPointed: typedData.value.pubIdPointed,
-    collectModule: typedData.value.collectModule,
-    collectModuleData: typedData.value.collectModuleData,
-    referenceModule: typedData.value.referenceModule,
-    referenceModuleData: typedData.value.referenceModuleData,
+  const tx = await lensPeriphery.setProfileMetadataURIWithSig({
+    user: address,
+    profileId: createProfileMetadataRequest.profileId,
+    metadata: createProfileMetadataRequest.metadata,
     sig: {
       v,
       r,
@@ -138,42 +112,20 @@ export const setUpdateProfile = async () => {
       deadline: typedData.value.deadline,
     },
   });
-  console.log('create comment: tx hash', tx.hash);
+  console.log('create profile metadata: tx hash', tx.hash);
 
-  console.log('create comment: poll until indexed');
+  console.log('create profile metadata: poll until indexed');
   const indexedResult = await pollUntilIndexed(tx.hash);
 
-  console.log('create comment: profile has been indexed', result);
+  console.log('create profile metadata: profile has been indexed', result);
 
   const logs = indexedResult.txReceipt.logs;
 
-  console.log('create comment: logs', logs);
-
-  const topicId = utils.id(
-    'CommentCreated(uint256,uint256,string,uint256,uint256,address,bytes,address,bytes,uint256)'
-  );
-  console.log('topicid we care about', topicId);
-
-  const profileCreatedLog = logs.find((l: any) => l.topics[0] === topicId);
-  console.log('create comment: created log', profileCreatedLog);
-
-  let profileCreatedEventLog = profileCreatedLog.topics;
-  console.log('create comment: created event logs', profileCreatedEventLog);
-
-  const publicationId = utils.defaultAbiCoder.decode(['uint256'], profileCreatedEventLog[2])[0];
-
-  console.log(
-    'create comment: contract publication id',
-    BigNumber.from(publicationId).toHexString()
-  );
-  console.log(
-    'create comment: internal publication id',
-    profileId + '-' + BigNumber.from(publicationId).toHexString()
-  );
+  console.log('create profile metadata: logs', logs);
 
   return result.data;
 };
 
 (async () => {
-  await setUpdateProfile();
+  await setProfileMetadataTypedData();
 })();
