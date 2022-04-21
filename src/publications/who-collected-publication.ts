2030ddd4cd5d72a8dedbe6d0dfc6bfeb6894fce4
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-04-21 12:08:43 +0100

    who collected publication docs

---
+++ src/publications/who-collected-publication.ts
@@ -0,0 +1,117 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { prettyJSON } from '../helpers';
+
+const WHO_COLLECTED = `
+  query($request: WhoCollectedPublicationRequest!) {
+    whoCollectedPublication(request: $request) {
+       items {
+        address
+        defaultProfile {
+          id
+          name
+          bio
+          location
+          website
+          twitter
+          isDefault
+          attributes {
+            displayType
+            traitType
+            key
+            value
+          }
+          metadata
+          handle
+          picture {
+            ... on NftImage {
+              contractAddress
+              tokenId
+              uri
+              chainId
+              verified
+            }
+            ... on MediaSet {
+              original {
+                url
+                mimeType
+              }
+            }
+          }
+          coverPicture {
+            ... on NftImage {
+              contractAddress
+              tokenId
+              uri
+              chainId
+              verified
+            }
+            ... on MediaSet {
+              original {
+                url
+                mimeType
+              }
+            }
+          }
+          ownedBy
+          depatcher {
+            address
+            canUseRelay
+          }
+          stats {
+            totalFollowers
+            totalFollowing
+            totalPosts
+            totalComments
+            totalMirrors
+            totalPublications
+            totalCollects
+          }
+          followModule {
+            ... on FeeFollowModuleSettings {
+              type
+              contractAddress
+              amount {
+                asset {
+                  name
+                  symbol
+                  decimals
+                  address
+                }
+                value
+              }
+              recipient
+            }
+          }
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+`;
+
+export const whoCollectedRequest = (publicationId: string) => {
+  return apolloClient.query({
+    query: gql(WHO_COLLECTED),
+    variables: {
+      request: {
+        publicationId,
+      },
+    },
+  });
+};
+
+export const whoCollected = async () => {
+  const result = await whoCollectedRequest('0x0f-0x01');
+  prettyJSON('who collected: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await whoCollected();
+})();
