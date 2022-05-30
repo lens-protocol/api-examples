1cd2764e7f9c48ae9cb92be29ef4be4f196f6b41
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-05-30 09:47:01 +0100

    feat: explore profiles

---
+++ src/explore/explore-profiles.ts
@@ -0,0 +1,120 @@
+import { gql } from '@apollo/client/core';
+import { apolloClient } from '../apollo-client';
+import { prettyJSON } from '../helpers';
+
+const EXPLORE_PROFILES = `
+  query($request: ExploreProfilesRequest!) {
+    exploreProfiles(request: $request) {
+      items {
+        id
+        name
+        bio
+        isDefault
+        attributes {
+          displayType
+          traitType
+          key
+          value
+        }
+        metadata
+        handle
+        picture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            chainId
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+        }
+        coverPicture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            chainId
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+        }
+        ownedBy
+        dispatcher {
+          address
+          canUseRelay
+        }
+        stats {
+          totalFollowers
+          totalFollowing
+          totalPosts
+          totalComments
+          totalMirrors
+          totalPublications
+          totalCollects
+        }
+        followModule {
+          ... on FeeFollowModuleSettings {
+            type
+            contractAddress
+            amount {
+              asset {
+                name
+                symbol
+                decimals
+                address
+              }
+              value
+            }
+            recipient
+          }
+          ... on ProfileFollowModuleSettings {
+          type
+          }
+          ... on RevertFollowModuleSettings {
+          type
+          }
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+`;
+
+// sort out types by generating them!
+export const exploreProfiles = (exploreProfilesQueryRequest: any) => {
+  return apolloClient.query({
+    query: gql(EXPLORE_PROFILES),
+    variables: {
+      request: exploreProfilesQueryRequest,
+    },
+  });
+};
+
+export const explore = async () => {
+  const result = await exploreProfiles({
+    sortCriteria: 'MOST_FOLLOWERS',
+    limit: 50,
+  });
+
+  prettyJSON('explore: result', result.data);
+
+  return result.data;
+};
+
+(async () => {
+  await explore();
+})();
