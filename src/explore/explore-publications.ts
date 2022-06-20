590b1b2b590f2fb02d20852339fabb2a9513772b
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-06-20 12:15:18 +0100

    feat: add docs for explore publication enums

--- src/explore/explore-publications.ts
+++ src/explore/explore-publications.ts
@@ -347,6 +347,8 @@ const explorePublications = (explorePublicationQueryRequest: {
 export const explore = async () => {
   const result = await explorePublications({
     // switch for `TOP_COLLECTED` if you wanted collected!
+    // switch for `TOP_MIRRORED` if you wanted top mirrored
+    // switch for `TOP_COMMENTED` if you wanted top commented
     sortCriteria: 'LATEST',
     limit: 50,
   });
