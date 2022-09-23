30db92cd2df52fe01c8e62ab96e3fa700248ec07
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-09-23 11:37:05 +0100

    create profle publication revenue

---
+++ src/revenue/profile-publications-revenue.ts
@@ -0,0 +1,29 @@
+import { apolloClient } from '../apollo-client';
+import {
+  ProfilePublicationRevenueDocument,
+  ProfilePublicationRevenueQueryRequest,
+} from '../graphql/generated';
+
+export const profilePublicationsRevenueRequest = async (
+  request: ProfilePublicationRevenueQueryRequest
+) => {
+  const result = await apolloClient.query({
+    query: ProfilePublicationRevenueDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data.profilePublicationRevenue;
+};
+
+export const profilePublicationsRevenue = async () => {
+  const result = await profilePublicationsRevenueRequest({ profileId: '0x41' });
+  console.log('publications for sale: result', result);
+
+  return result;
+};
+
+(async () => {
+  await profilePublicationsRevenue();
+})();
