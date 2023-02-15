1bc7828d32f77d0705cee980f43384d9f0bd15f3
Zannis <zannis@users.noreply.github.com>
2023-02-15 12:54:53 +0200

    Feat: multirecipient collect module example (#111)
    * add example for multirecipient fee collect module
    
    * update some bignumber arithmetic
    
    * update example to latest valid format
    
    * remove timeline document and example as it's deprecated
    
    * add aave and erc4626 examples
    
    * restore collect pub id

--- src/publications/post.ts
+++ src/publications/post.ts
@@ -88,6 +88,49 @@ const createPost = async () => {
       //   recipient: '0x3A5bd1E37b099aE3386D13947b6a90d97675e5e3',
       //   referralFee: 0,
       // },
+      // multirecipientFeeCollectModule: {
+      //   amount: {
+      //     currency: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+      //     value: '0.001',
+      //   },
+      //   collectLimit: '2',
+      //   endTimestamp: '2025-01-01T00:00:00.000Z',
+      //   referralFee: 0,
+      //   followerOnly: false,
+      //   recipients: [
+      //     {
+      //       recipient: address,
+      //       split: 50,
+      //     },
+      //     {
+      //       recipient: '0xacab2c2Cdde3a5839b91BABEfFd5fd5128590d6f',
+      //       split: 50,
+      //     },
+      //   ],
+      // },
+      // aaveFeeCollectModule: {
+      //   amount: {
+      //     currency: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+      //     value: '0.001',
+      //   },
+      //   collectLimit: '2',
+      //   endTimestamp: '2025-01-01T00:00:00.000Z',
+      //   referralFee: 0,
+      //   followerOnly: false,
+      //   recipient: address,
+      // },
+      // erc4626FeeCollectModule: {
+      //   amount: {
+      //     currency: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+      //     value: '0.001',
+      //   },
+      //   collectLimit: '2',
+      //   endTimestamp: '2025-01-01T00:00:00.000Z',
+      //   referralFee: 0,
+      //   followerOnly: false,
+      //   recipient: address,
+      //   vault: '0x3A5bd1E37b099aE3386D13947b6a90d97675e5e3', // MUST BE VALID ERC4626 VAULT
+      // },
     },
     referenceModule: {
       followerOnlyReferenceModule: false,
