9c1623e59f172db84ac149280518bfc2022e43d1
Josh Stevens <joshstevens19@hotmail.co.uk>
2023-09-02 09:34:54 +0100

    feat: comment onchain

---
+++ src/publications/comment-onchain-lens-profile-manager.ts
@@ -0,0 +1,62 @@
+import { apolloClient } from '../apollo-client';
+import { login } from '../authentication/login';
+import { explicitStart } from '../config';
+import { getAddressFromSigner } from '../ethers.service';
+import { CommentOnchainDocument, OnchainCommentRequest } from '../graphql/generated';
+import { uploadIpfs } from '../ipfs';
+import { waitUntilLensManagerTransactionIsComplete } from '../transaction/wait-until-complete';
+
+const commentOnChain = async (request: OnchainCommentRequest) => {
+  const result = await apolloClient.mutate({
+    mutation: CommentOnchainDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data!.commentOnchain;
+};
+
+export const CommentOnChainLensProfileManager = async (profileId: string = '0x02') => {
+  const address = getAddressFromSigner();
+  console.log('comment onchain lens profile manager: address', address);
+
+  await login(address);
+
+  // TODO! USE METADATA PACKAGE FOR NICE TYPINGS
+  const ipfsResult = await uploadIpfs<any>({
+    $schema:
+      'https://raw.githubusercontent.com/lens-protocol/LIPs/feat/metadata-standards/lens-metadata-standards/publication/text-only/1.0.0/schema.json',
+    name: 'My text',
+    description: 'My text Description',
+    external_url: 'https://mytext.com',
+    attributes: [],
+    image: 'https://text.com/image.png',
+    lens: {
+      title: 'My text',
+      id: '1030ee6e-51cb-4a09-a74a-abdccc6ef890',
+      locale: 'en-US',
+      mainContentFocus: 'TEXT_ONLY',
+      content: 'My text Content',
+      tags: ['text'],
+      appId: 'my-app-id',
+    },
+  });
+  console.log('comment onchain: ipfs result', ipfsResult);
+
+  // TODO! in docs make sure we talk about onchain referrals
+  const request: OnchainCommentRequest = {
+    commentOn: '0x03-0x03',
+    contentURI: `ipfs://${ipfsResult.path}`,
+  };
+
+  const result = await commentOnChain(request);
+  console.log('comment onchain lens profile manager: result', result);
+  await waitUntilLensManagerTransactionIsComplete(result, 'unblock');
+};
+
+(async () => {
+  if (explicitStart(__filename)) {
+    await CommentOnChainLensProfileManager();
+  }
+})();
---
+++ src/publications/comment-onchain.ts
@@ -0,0 +1,106 @@
+import { apolloClient } from '../apollo-client';
+import { login } from '../authentication/login';
+import { broadcastOnchainRequest } from '../broadcast/shared-broadcast';
+import { explicitStart, PROFILE_ID, USE_GASLESS } from '../config';
+import { getAddressFromSigner, signedTypeData, splitSignature } from '../ethers.service';
+import { CreateOnchainCommentTypedDataDocument, OnchainCommentRequest } from '../graphql/generated';
+import { uploadIpfs } from '../ipfs';
+import { lensHub } from '../lens-hub';
+import { waitUntilBroadcastTransactionIsComplete } from '../transaction/wait-until-complete';
+
+export const createOnchainCommentTypedData = async (request: OnchainCommentRequest) => {
+  const result = await apolloClient.mutate({
+    mutation: CreateOnchainCommentTypedDataDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data!.createOnchainCommentTypedData;
+};
+
+const commentOnChain = async () => {
+  const profileId = PROFILE_ID;
+  if (!profileId) {
+    throw new Error('Must define PROFILE_ID in the .env to run this');
+  }
+
+  const address = getAddressFromSigner();
+  console.log('comment onchain: address', address);
+
+  await login(address);
+
+  // TODO! USE METADATA PACKAGE FOR NICE TYPINGS
+  const ipfsResult = await uploadIpfs<any>({
+    $schema:
+      'https://raw.githubusercontent.com/lens-protocol/LIPs/feat/metadata-standards/lens-metadata-standards/publication/text-only/1.0.0/schema.json',
+    name: 'My text',
+    description: 'My text Description',
+    external_url: 'https://mytext.com',
+    attributes: [],
+    image: 'https://text.com/image.png',
+    lens: {
+      title: 'My text',
+      id: '1030ee6e-51cb-4a09-a74a-abdccc6ef890',
+      locale: 'en-US',
+      mainContentFocus: 'TEXT_ONLY',
+      content: 'My text Content',
+      tags: ['text'],
+      appId: 'my-app-id',
+    },
+  });
+  console.log('comment onchain: ipfs result', ipfsResult);
+
+  // TODO! in docs make sure we talk about onchain referrals
+  const request: OnchainCommentRequest = {
+    commentOn: '0x03-0x03',
+    contentURI: `ipfs://${ipfsResult.path}`,
+  };
+
+  const { id, typedData } = await createOnchainCommentTypedData(request);
+  console.log('comment onchain: result', { id, typedData });
+
+  console.log('comment onchain: typedData', typedData);
+
+  const signature = await signedTypeData(typedData.domain, typedData.types, typedData.value);
+  console.log('comment onchain: signature', signature);
+
+  if (USE_GASLESS) {
+    const broadcastResult = await broadcastOnchainRequest({ id, signature });
+
+    await waitUntilBroadcastTransactionIsComplete(broadcastResult, 'Comment');
+  } else {
+    const { v, r, s } = splitSignature(signature);
+
+    const tx = await lensHub.commentWithSig(
+      {
+        profileId: typedData.value.profileId,
+        contentURI: typedData.value.contentURI,
+        pointedProfileId: typedData.value.pointedProfileId,
+        pointedPubId: typedData.value.pointedPubId,
+        referrerProfileIds: typedData.value.referrerProfileIds,
+        referrerPubIds: typedData.value.referrerPubIds,
+        referenceModuleData: typedData.value.referenceModuleData,
+        actionModules: typedData.value.actionModules,
+        actionModulesInitDatas: typedData.value.actionModulesInitDatas,
+        referenceModule: typedData.value.referenceModule,
+        referenceModuleInitData: typedData.value.referenceModuleInitData,
+      },
+      {
+        signer: address,
+        v,
+        r,
+        s,
+        deadline: typedData.value.deadline,
+      },
+      { gasLimit: 10000000 }
+    );
+    console.log('comment onchain: tx hash', tx.hash);
+  }
+};
+
+(async () => {
+  if (explicitStart(__filename)) {
+    await commentOnChain();
+  }
+})();
