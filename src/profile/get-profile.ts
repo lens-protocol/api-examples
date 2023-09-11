97cfc4fce86d2cd1eabe446d995419b6776f4168
Yahya <ya7yaz@gmail.com>
2023-09-11 13:23:04 +0200

    get profile

--- src/profile/get-profile.ts
+++ src/profile/get-profile.ts
@@ -2,20 +2,24 @@ import { apolloClient } from '../apollo-client';
 import { login } from '../authentication/login';
 import { explicitStart, PROFILE_ID } from '../config';
 import { getAddressFromSigner } from '../ethers.service';
-import { ProfileDocument, SingleProfileQueryRequest } from '../../graphql-v1/generated';
+// import { ProfileDocument, SingleProfileQueryRequest } from '../../graphql-v1/generated';
+import { ProfileDocument, ProfileRequest } from '../graphql/generated';
 
-const getProfileRequest = async (request: SingleProfileQueryRequest) => {
+const getProfileRequest = async (request: ProfileRequest) => {
   const result = await apolloClient.query({
     query: ProfileDocument,
     variables: {
       request,
+      statsRequest: {},
+      countOpenActionsRequest: {},
+      reactionsRequest: {},
     },
   });
 
   return result.data.profile;
 };
 
-export const profile = async (request?: SingleProfileQueryRequest) => {
+export const profile = async (request?: ProfileRequest) => {
   const profileId = PROFILE_ID;
   if (!profileId) {
     throw new Error('Must define PROFILE_ID in the .env to run this');
@@ -27,7 +31,7 @@ export const profile = async (request?: SingleProfileQueryRequest) => {
   await login(address);
 
   if (!request) {
-    request = { profileId: '0x09' };
+    request = { profileId: '0x07' };
   }
 
   const profile = await getProfileRequest(request);
