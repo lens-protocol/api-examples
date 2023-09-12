43dfecd6daa750f83fdaaaeb4ac63f23c0ee2b7d
Yahya <ya7yaz@gmail.com>
2023-09-12 17:31:22 +0200

    who collected

--- src/publications/who-collected-publication.ts
+++ src/publications/who-collected-publication.ts
@@ -1,22 +1,35 @@
 import { apolloClient } from '../apollo-client';
 import {
-  WhoCollectedPublicationDocument,
-  WhoCollectedPublicationRequest,
-} from '../../graphql-v1/generated';
+  OpenActionCategoryType,
+  WhoActedOnPublicationDocument,
+  WhoActedOnPublicationRequest,
+} from '../graphql/generated';
 
-export const whoCollectedRequest = async (request: WhoCollectedPublicationRequest) => {
+export const whoCollectedRequest = async (request: WhoActedOnPublicationRequest) => {
   const result = await apolloClient.query({
-    query: WhoCollectedPublicationDocument,
+    query: WhoActedOnPublicationDocument,
     variables: {
       request,
+      statsRequest: {},
+      countOpenActionsRequest: {},
+      reactionsRequest: {},
     },
   });
 
-  return result.data.whoCollectedPublication;
+  return result.data.whoActedOnPublication;
 };
 
 export const whoCollected = async () => {
-  const result = await whoCollectedRequest({ publicationId: '0x41-0x03' });
+  const result = await whoCollectedRequest({
+    on: '0x41-0x03',
+    where: {
+      anyOf: [
+        {
+          category: OpenActionCategoryType.Collect,
+        },
+      ],
+    },
+  });
   console.log('who collected: result', result);
 
   return result;
