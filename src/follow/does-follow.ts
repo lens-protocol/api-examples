61afba9871a57af6f2e89cacc62b0186a28ce352
Yahya <ya7yaz@gmail.com>
2023-09-14 11:29:51 +0200

    paginated does-follow

--- src/follow/does-follow.ts
+++ src/follow/does-follow.ts
@@ -15,16 +15,10 @@ const doesFollowRequest = async (request: DoesFollowRequest) => {
 
 export const doesFollow = async () => {
   // Note this endpoint depends on PR https://github.com/lens-protocol/lens-monorepo/pull/182 on the api side
-  const followInfos = [
-    {
-      followerAddress: '0xEEA0C1f5ab0159dba749Dc0BAee462E5e293daaF',
-      profileId: '0x02',
-    },
-  ];
 
   const result = await doesFollowRequest({
-    following: knownProfileId,
-    follower: followerProfileId,
+    for: knownProfileId,
+    followers: [followerProfileId],
   });
   console.log('does follow: result', result);
 
