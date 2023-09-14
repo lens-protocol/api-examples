69e05d292f89baf51e84c479ab3429f101fbc15b
Josh Stevens <joshstevens19@hotmail.co.uk>
2023-09-14 22:39:04 +0100

    feat: act on lens manager + fix

--- src/open-action/act-on-open-action.ts
+++ src/open-action/act-on-open-action.ts
@@ -1,40 +1,76 @@
 import { apolloClient } from '../apollo-client';
 import { login } from '../authentication/login';
-import { getAddressFromSigner } from '../ethers.service';
+import { broadcastOnchainRequest } from '../broadcast/shared-broadcast';
+import { USE_GASLESS, explicitStart } from '../config';
+import { getAddressFromSigner, signedTypeData, splitSignature } from '../ethers.service';
 import {
-  ActOnOpenActionDocument,
+  ActOnOpenActionRequest,
   CreateActOnOpenActionTypedDataDocument,
 } from '../graphql/generated';
+import { lensHub } from '../lens-hub';
+import { waitUntilBroadcastTransactionIsComplete } from '../transaction/wait-until-complete';
 
-(async function () {
+export const createActOnOpenActionTypedData = async (request: ActOnOpenActionRequest) => {
+  const result = await apolloClient.mutate({
+    mutation: CreateActOnOpenActionTypedDataDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data!.createActOnOpenActionTypedData;
+};
+
+export const actOn = async () => {
   const address = getAddressFromSigner();
-  console.log('act on open action: address', address);
+  console.log('act on: address', address);
 
   await login(address);
 
-  const createOpenActionRes = await apolloClient.mutate({
-    mutation: CreateActOnOpenActionTypedDataDocument,
-    variables: {
-      request: {
-        for: '0x01-0x01',
-        actOn: {
-          simpleCollectOpenAction: true,
-        },
-      },
+  const { id, typedData } = await createActOnOpenActionTypedData({
+    for: '0x03-0x4b',
+    actOn: {
+      simpleCollectOpenAction: true,
     },
   });
+  console.log('act on: result', { id, typedData });
+  console.log('act on: typedData', typedData);
 
-  console.info('create open action result', createOpenActionRes);
+  const signature = await signedTypeData(typedData.domain, typedData.types, typedData.value);
+  console.log('act on: signature', signature);
 
-  await apolloClient.mutate({
-    mutation: ActOnOpenActionDocument,
-    variables: {
-      request: {
-        for: '0x01-0x01',
-        actOn: {
-          simpleCollectOpenAction: true,
-        },
+  if (USE_GASLESS) {
+    const broadcastResult = await broadcastOnchainRequest({ id, signature });
+
+    await waitUntilBroadcastTransactionIsComplete(broadcastResult, 'act on');
+  } else {
+    const { v, r, s } = splitSignature(signature);
+
+    const tx = await lensHub.actWithSig(
+      {
+        publicationActedProfileId: typedData.value.publicationActedProfileId,
+        publicationActedId: typedData.value.publicationActedId,
+        actorProfileId: typedData.value.actorProfileId,
+        referrerProfileIds: typedData.value.referrerProfileIds,
+        referrerPubIds: typedData.value.referrerPubIds,
+        actionModuleAddress: typedData.value.actionModuleAddress,
+        actionModuleData: typedData.value.actionModuleData,
       },
-    },
-  });
+      {
+        signer: address,
+        v,
+        r,
+        s,
+        deadline: typedData.value.deadline,
+      }
+    );
+    console.log('act on: tx hash', tx.hash);
+    return tx.hash;
+  }
+};
+
+(async () => {
+  if (explicitStart(__filename)) {
+    await actOn();
+  }
 })();
